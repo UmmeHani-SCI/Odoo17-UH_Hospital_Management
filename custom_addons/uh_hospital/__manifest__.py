b3637c9902ac87467231baf27fc234e04cbcc5aa
umme.hani <umme.hani@savethechildren.org>
2024-09-30 11:10:27 +0600

    Initial Commit

--- custom_addons/uh_hospital/__manifest__.py
+++ custom_addons/uh_hospital/__manifest__.py
@@ -7,17 +7,25 @@
     "License" :"LGPL-3",
     'version' :"17.0.1.1",
     'category': 'Healthcare',
-    'depends': ['base','web'],
+    'depends': ['base','web', 'mail'],
     'demo': [
         'demo/demo.xml',
     ],
     'data': ['security/ir.model.access.csv',  # Access control file
+             'views/patients_views.xml',
+             'views/patients_readonly_views.xml',
              'views/menu.xml',
+
 
     ],
 
     'installable': True,
     'application': True,
     'auto_install': False,
+    'assets': {
+        'web.assets_backend': [
+
+               ],
+},
 
 }
--- custom_addons/uh_hospital/models/hospital_patient.py
+++ custom_addons/uh_hospital/models/hospital_patient.py
@@ -1,19 +1,32 @@
 
 from odoo import api, fields, models
-
+from datetime import date
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
     _description = 'Patient Master'
+    _inherit =  'mail.thread'
 
-    name = fields.Char(string="Name", required=True)
-    age = fields.Integer(string="Age", required=True)
+    name = fields.Char(string='Name', required=True, tracking=True)
+    dob = fields.Date(string='Date of Birth', tracking=True)
+    age = fields.Integer(string='Age', compute='_compute_age', store=True, tracking=True)
+
     gender = fields.Selection([
         ('male', 'Male'),
         ('female', 'Female'),
         ('other', 'Other'),
-    ], string="Gender", required=True)
-    date_of_birth = fields.Date(string="Date of Birth")
-    phone = fields.Char(string="Phone Number")
-    email = fields.Char(string="Email")
-    address = fields.Text(string="Address")
+    ], string='Gender', required=True , tracking=True)
+    date_of_admitted_to_hospital = fields.Date(string='Date of Admitted To Hospital / Checkup', tracking=True)
+    phone = fields.Char(string='Phone Number',  tracking=True)
+    email = fields.Char(string='Email' , tracking=True)
+    address = fields.Text(string='Address' , tracking=True)
+
+    @api.depends('dob')
+    def _compute_age(self):
+        for record in self:
+            if record.dob:
+                today = date.today()
+                dob = fields.Date.from_string(record.dob)
+                record.age = today.year - dob.year - ((today.month, today.day) < (dob.month, dob.day))
+            else:
+                record.age = 0
